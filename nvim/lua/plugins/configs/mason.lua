c37c7fa4ef1da8bd0f70bd5b9f88ee720c820684
Guillem Ballesteros <guillem.ballesteros.garcia@gmail.com>
2022-07-24 19:48:26 +0100

    Move mason config to its own file.

---
+++ nvim/lua/plugins/configs/mason.lua
@@ -0,0 +1,7 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "sumneko_lua", "rust_analyzer", "pyright" },
+})
+require("mason-tool-installer").setup({
+  ensure_installed = { "stylua", "black", "isort" },
+})
--- nvim/lua/plugins/configs/misc.lua
+++ nvim/lua/plugins/configs/misc.lua
@@ -118,14 +118,3 @@ require("nvim-navic").setup({
     TypeParameter = " ",
   },
 })
-
------------
--- Mason --
------------
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "rust_analyzer" },
-})
-require("mason-tool-installer").setup({
-  ensure_installed = { "stylua", "black", "isort", "rust-analyzer", "pyright" },
-})
