4c515d86c828d454f3227b8f09335f74e5983f1b
Guillem Ballesteros <guillem@kuvaspace.com>
2022-07-25 12:15:40 +0100

    Make cursor black for catpuccin.

--- nvim/lua/colors/catppuccin.lua
+++ nvim/lua/colors/catppuccin.lua
@@ -18,3 +18,12 @@ require("catppuccin").setup({
 })
 
 vim.api.nvim_command "colorscheme catppuccin"
+
+vim.cmd [[
+ highlight Cursor guifg=white guibg=black
+ highlight iCursor guifg=white guibg=steelblue
+ set guicursor=n-v-c:block-Cursor
+ set guicursor+=i:ver100-iCursor
+ set guicursor+=n-v-c:blinkon0
+ set guicursor+=i:blinkwait10
+]]
