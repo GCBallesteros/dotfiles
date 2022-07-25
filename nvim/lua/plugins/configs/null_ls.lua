7d482d7e9f293a4ad4fffd40cbfd237f493ad9a3
Guillem Ballesteros <guillem@kuvaspace.com>
2022-07-25 12:15:22 +0100

    Add flake8 to mason and null-ls.

--- nvim/lua/plugins/configs/null_ls.lua
+++ nvim/lua/plugins/configs/null_ls.lua
@@ -5,7 +5,7 @@ null_ls.setup({
     null_ls.builtins.formatting.stylua.with({
       extra_args = { "--indent-type", "Spaces", "--indent-width", "2", "--call-parentheses", "NoSingleString" },
     }),
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.flake8.with({extra_args={"--ignore-errors", "E402"}}),
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
   },
