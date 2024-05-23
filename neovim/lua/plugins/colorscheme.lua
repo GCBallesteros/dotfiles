802212833258dc7768991e42819bc310aeb12250
GCBallesteros <dev+github@maxwellrules.com>
2024-05-23 12:56:09 +0200

    Update the config and remove Catpuccin since it's default on LazyVim

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
   { "rose-pine/neovim", priority = 1000, name = "rose-pine", opts = { variant = "moon" } },
-  { "catppuccin", priority = 1000, opts = { integrations = { leap = true, symbols_outline = true } } },
   { "xero/miasma.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
